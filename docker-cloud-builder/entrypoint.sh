c0b433123ba80ccf2cce213687761e004cf17344
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2020-02-26 09:22:29 +0530

    Trying to get buildx to use 'docker-container' as the driver.

--- docker-cloud-builder/entrypoint.sh
+++ docker-cloud-builder/entrypoint.sh
@@ -32,7 +32,7 @@ buildxInitialise() {
   # If the `buildx` executable is in PATH
   if [ $(which "${buildxCommand}") ] ; then
     # Initialise a builder and switch to it
-    "${buildxCommand}" create --driver docker-container --driver-opt image=moby/buildkit:rootless \
+    "${buildxCommand}" create --driver docker-container --driver-opt image=moby/buildkit:master \
       --name multiarch-builder --use \
       && "${buildxCommand}" inspect --bootstrap
   else
