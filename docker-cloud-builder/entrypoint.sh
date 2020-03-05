e23d738a93cc82202d084122c5da159d841c5a75
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2020-03-05 23:30:15 +0530

    Changes to Dockerfile
    Have added pinned versions of Alpine Linux, and other packages or images
    used in the Dockerfile. Same goes for the entrypoint.sh file which has
    been improved. Now, the Dockerfile and entrypoint have been structured
    such that any command passed to the builder will be same as passing
    arguments to the 'docker' command. Now, 'docker build' will invoke the
    'buildx' builder instead of the old builder. Have also added some tests
    to the container-structure-test file. Finally, have added tags with
    pinned version number corresponding to the version of docker installed
    in the image.

--- docker-cloud-builder/entrypoint.sh
+++ docker-cloud-builder/entrypoint.sh
@@ -11,16 +11,19 @@
 #                 and run as the ENTRYPOINT command.
 ################
 
-# Variable that contains name of `buildx` executable
-buildxCommand="docker-buildx"
-binfmtVersion="0.7"
 
 # Function which initialises `buildx`
 buildxInitialise() {
+  # Variable that contains name of `buildx` executable
+  buildxCommand="docker-buildx"
+
+  # Variable that pins the binfmt image version
+  binfmtVersion="latest"
+
   # Running the below command adds support for multi-arch
   # builds by setting up QEMU
-  docker run --privileged harshavardhanj/binfmt:latest || exit 1
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker run --privileged harshavardhanj/binfmt:"${binfmtVersion}" || exit 1
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes || exit 1
 
   # If the `buildx` executable is in PATH
   if [ "$(command -v "${buildxCommand}")" ] ; then
@@ -32,13 +35,38 @@ buildxInitialise() {
     printf '%s\n' "The executable '${buildxCommand}' could not be found in the PATH." \
       && exit 1
   fi
+
+  # Command which "installs" buildx so that when `docker build` is called,
+  # 'buildx' is automatically used instead of the old builder.
+  "${buildxCommand}" install || exit 1
 }
 
+
 # Main function
+# This function does one of the following, depending on the argument(s)
+#     * Initialises 'buildx'
+#     * Initialises 'buildx' and passes all input arguments to
+#       the 'docker' command
 main() {
-  # Calling the initialisation function and passing all arguments to buildx
-  buildxInitialise \
-    && "${buildxCommand}" $@
+  # Name of docker executable
+  dockerCommand="docker"
+
+  # Argument which, when passed, begins ONLY the docker-buildx init process
+  initialisationArgument="init"
+
+  # If the only arguments passed IS the string defined in ${initialisationArgument}
+  if [ $# -eq 1 && "$1" = "${initialisationArgument}" ] ; then
+    buildxInitialise
+  # If multiple arguments are passed and the first one is NOT the string defined in
+  # ${initialisationArgument}
+  elif [ $# -gt 1 && "$1" != "${initialisationArgument}" ] ; then
+    buildxInitialise \
+      && "${dockerCommand}" $@
+  else
+    printf '%s\n' "Incorrect argument(s) '$@' received. Expecting either '${initialisationArgument}'\
+      or other arguments which '${dockerCommand}' accepts." >&2 \
+      && exit 1
+  fi
 }
 
 # Calling the main function and passing all arguments to it
